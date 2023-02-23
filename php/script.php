96d4a1c74eb14c8ade6a10172f33bd05bbbcc3ec
Edouard de Romree de Vichenet <deromree.edouard@gmail.com>
2023-02-23 10:28:14 +0100

    test deploy on webhost

--- php/script.php
+++ php/script.php
@@ -1,39 +1,56 @@
 <?php 
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $firstname = $_POST['firstname'];
-    $email = $_POST['email'];
-    $comment = $_POST['comment'];
+require 'connexion.php';
 
-  
-    $valid = true;
-  
-    if (!preg_match('/^[a-zA-ZÀ-ÿ|\s]{2,255}$/u', $name)) {
+
+
+$name = trim($_POST['name']);
+$firstname = trim($_POST['firstname']);
+$email = trim($_POST['email']);
+$comment = trim($_POST['comment']);
+$valid = true;
+
+if (isset($_POST['submit'])) {
+  if (empty($name)) 
+  {
+    $nameerror= "<p> Le nom est obligatoire</p>";
+    if (!htmlspecialchars($name)) 
+    {
       $nameerror= "<p> Le nom est invalide</p>";
       $valid = false;
     } 
-    if (!preg_match('/^[a-zA-Z]{2,255}$/', $firstname)) {
+  } if (empty($firstname))
+  {
+    $firstnameerror= "<p> Le prénom est obligatoire</p>";
+    if (!htmlspecialchars($firstname))
+    {
       $firstnameerror="<p> Le prénom est invalide </p>";
       $valid = false;
     }
-    if (!preg_match('/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/', $email)) {
+  }
+  if (empty($email))
+  {
+    $emailerror= "<p>L'adresse email est obligatoire</p>";
+    if (!preg_match('/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/', $email))
+    {
+      $email= filter_var($email, FILTER_SANITIZE_EMAIL);
       $emailerror="<p>L'adresse email est invalide</p>";
       $valid = false;
     }
+  }
+  if (empty($comment)){
+    $commenterror= "<p>Le commentaire est obligatoire</p>";
     if (!preg_match('/^[a-zA-Z]{250,1000}$/', $comment)) {
       $commenterror= "<p>Le commentaire ne respecte pas les limites de caractères</p>";
       $valid = false;
     }
-  
-    if ($valid) {
-      $name = filter_var($name, FILTER_SANITIZE_STRING);
-      $firstname = filter_var($firstname, FILTER_SANITIZE_STRING);
-      $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-      $comment = filter_var($comment, FILTER_SANITIZE_STRING);
-
-      echo "Formulaire envoyé ";
-    }
   }
+}
 
 
+  //honeypot
+
+  if (!empty($_POST['email_confirm'])){
+    die('Une erreur est survenue. Veuillez réessayer plus tard.');
+  }
+  
