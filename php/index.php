93ea265cf36ddae2411bc1b1f3a54478b885ff3c
Edouard de Romree de Vichenet <deromree.edouard@gmail.com>
2023-02-23 08:27:24 +0100

    test

--- php/index.php
+++ php/index.php
@@ -1,7 +1,7 @@
 <?php 
-require '/wamp64/www/variable/Hackers-Poulette/php/connexion.php';
-require '/wamp64/www/variable/Hackers-Poulette/php/script.php';
-require '/wamp64/www/variable/Hackers-Poulette/php/add.php';
+require 'connexion.php';
+require 'script.php';
+require 'add.php';
 ?>
     <!DOCTYPE html>
 <html lang="en">
@@ -9,7 +9,7 @@ require '/wamp64/www/variable/Hackers-Poulette/php/add.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type='text/css' href="/assets/css/style.css">
+    <link rel="stylesheet" type='text/css' href="../assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Heebo&display=swap" rel="stylesheet">
